276a14796bdaaeae736cfb8b612671dfa05f98ef
anshu638 <anshu@nextsavy.com>
2024-12-19 18:38:09 +0530

    background image added that scrolls with parallax widget

--- lib/fifth/fifth_ui.dart
+++ lib/fifth/fifth_ui.dart
@@ -10,7 +10,9 @@ class FifthUi extends StatefulWidget {
 
 class _FifthUiState extends State<FifthUi> {
   late PageController _pageController;
+  late PageController _pageBackgroundController;
   double page = 0.0;
+  double bkgPage = 0.0;
 
   List<String> images = [
     'assets/images/fifth/img1.jpg',
@@ -29,12 +31,17 @@ class _FifthUiState extends State<FifthUi> {
       initialPage: 0,
       viewportFraction: 0.5,
     );
+    _pageBackgroundController = PageController(
+      initialPage: 0,
+    );
 
     _pageController.addListener(_onScroll);
     super.initState();
   }
 
   void _onScroll() {
+    _pageBackgroundController.animateToPage(_pageController.page!.toInt(),
+        duration: const Duration(milliseconds: 400), curve: Curves.easeOutQuad);
     setState(() {
       page = _pageController.page!;
     });
@@ -44,26 +51,44 @@ class _FifthUiState extends State<FifthUi> {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: SizedBox(
-          height: screenSize.width / 1.5,
-          child: PageView.builder(
-            controller: _pageController,
-            itemBuilder: (context, index) {
-              return SizedBox(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ParallaxImage(
-                    url: images[index],
-                    horizontalSlide: (index - page).clamp(-1, 1).toDouble(),
-                  ),
-                ),
-              );
-            },
-            itemCount: images.length,
+      body: Stack(
+        children: [
+          PageView.builder(
+              controller: _pageBackgroundController,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: double.maxFinite,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      image: DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage(
+                            images[index],
+                          ))),
+                );
+              }),
+          Center(
+            child: SizedBox(
+              height: screenSize.width / 1.5,
+              child: PageView.builder(
+                controller: _pageController,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ParallaxImage(
+                        url: images[index],
+                        horizontalSlide: (index - page).clamp(-1, 1).toDouble(),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: images.length,
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
